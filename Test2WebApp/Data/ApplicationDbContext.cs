29f5e1f17dea051503aed9200f5afc937152b9b9
seeram_rakshit <seeramrakshit@gmail.com>
2023-06-20 19:16:04 +0530

    Added some required files.

---
+++ Test2WebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Scaffolding.Internal;
+
+namespace Test2WebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ Test2WebApp/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Test2WebApp.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required] public string Name { get; set; }
+        public int DisplayOder { get; set; }
+    }
+}
