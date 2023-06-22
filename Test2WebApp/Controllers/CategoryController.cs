263dd2300f0f50577f18c7e4299e54e83a05b17e
seeram_rakshit <seeramrakshit@gmail.com>
2023-06-22 09:59:30 +0530

    Added Create,Edit,Delete views of categories.

--- Test2WebApp/Controllers/CategoryController.cs
+++ Test2WebApp/Controllers/CategoryController.cs
@@ -15,5 +15,85 @@ namespace Test2WebApp.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category cat)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(cat);
+                _db.SaveChanges();
+                TempData["success"] = "Category Created Successfully!";
+                return RedirectToAction("Index");
+            }
+            TempData["error"] = "Category Create is not done";
+            return View();
+            
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? catFromDb = _db.Categories.Find(id);
+            if (catFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(catFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category cat)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(cat);
+                _db.SaveChanges();
+                TempData["success"] = "Category Updated Successfully!";
+                return RedirectToAction("Index");
+            }
+            TempData["error"] = "Category Update is not done";
+            return View();
+
+        }
+
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return RedirectToAction("Index");
+            }
+            Category? catFromDb = _db.Categories.Find(id);
+            if (catFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(catFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category? cat= _db.Categories.Find(id);
+            if (cat == null)
+            {
+                TempData["error"] = "Category Delete is not done";
+                return Index();
+            }
+            _db.Categories.Remove(cat);
+            _db.SaveChanges();
+            TempData["success"] = "Category Deleted Successfully!";
+            return RedirectToAction("Index");
+
+        }
     }
 }
